b8cce36a1d584c12f00d0f011218b7a619981f88
Lockandroll <mckeont39@gmail.com>
2026-06-10 13:09:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const { initDB } = require('./db');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/cities', require('./routes/cities'));
+app.use('/api/settings', require('./routes/settings'));
 app.use('/api/pos', require('./routes/pos'));
 
 // Catch-all: serve frontend
