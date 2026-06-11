cc56de6e6ff5b7d94054c3da11306f9c69f5e0ec
Lockandroll <mckeont39@gmail.com>
2026-06-11 15:23:46 -0400

    Create vendors.js

---
+++ routes/vendors.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const { initDB } = require('./db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/cities', require('./routes/cities'));
+app.use('/api/settings', require('./routes/settings'));
+app.use('/api/pos', require('./routes/pos'));
+app.use('/api/quotes', require('./routes/quotes'));
+app.use('/api/addresses', require('./routes/addresses'));
+app.use('/api/vendors', require('./routes/vendors'));
+
+// Catch-all: serve frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('PO System running on port ' + PORT));
+
+initDB()
+  .then(() => console.log('Database initialized'))
+  .catch(err => console.error('DB init error (non-fatal):', err));
